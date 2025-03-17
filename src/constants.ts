5a6659cce84bbf87de31548a9c38d5ca59656849
Mason Daugherty <mdaugher@bowdoin.edu>
2025-03-16 20:36:27 -0400

    fix: update Spinitron home page URL to include layout parameter

--- src/constants.ts
+++ src/constants.ts
@@ -7,7 +7,7 @@ export const LOGO_URL = "https://wbor.org/assets/images/apple-touch-icon.png";
 
 export const SSE_TRACK_FEED = "https://azura.wbor.org/api/live/nowplaying/sse";
 
-export const SPINITRON_HOME_PAGE_URL = "https://playlists.wbor.org/WBOR/";
+export const SPINITRON_HOME_PAGE_URL = "https://playlists.wbor.org/WBOR/?layout=1";
 
 // The station ID for AzuraCast
 export const STATION_ID = "station:wbor";
